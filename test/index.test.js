b2eed4137ae3d2b29a648ac05178f3d7c99c9d17
Justin Fuller <justindanielfuller@gmail.com>
2019-04-13 10:13:01 -0400

    Add test cases

--- test/index.test.js
+++ test/index.test.js
@@ -8,3 +8,22 @@ test('Even, unordered', function (t) {
 
   t.deepEqual(mergeSort(input), expected)
 })
+
+test('Odd, unordered', function (t) {
+  const input = [5, 10, 3, 1, 2, 4, 12]
+  const expected = [1, 2, 3, 4, 5, 10, 12]
+
+  t.deepEqual(mergeSort(input), expected)
+})
+
+test('Even, ordered', function (t) {
+  const input = [1, 2, 3, 4, 5, 10, 12, 15]
+
+  t.deepEqual(mergeSort(input), input)
+})
+
+test('Odd, ordered', function (t) {
+  const input = [1, 2, 3, 4, 5, 10, 12]
+
+  t.deepEqual(mergeSort(input), input)
+})
