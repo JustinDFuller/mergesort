2e6629d97257ed3d361f9061bd22152e3e16ec34
Justin Fuller <justindanielfuller@gmail.com>
2019-04-13 10:07:15 -0400

    Basic scenario working.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-function merge (head, tail) {}
+function merge (head, tail) {
+  const sortedArray = []
+
+  let headIndex = 0
+  let tailIndex = 0
+
+  while (headIndex < head.length && tailIndex < tail.length) {
+    if (head[headIndex] < tail[tailIndex]) {
+      sortedArray.push(head[headIndex++])
+    } else {
+      sortedArray.push(tail[tailIndex++])
+    }
+  }
+
+  while (headIndex < head.length) {
+    sortedArray.push(head[headIndex++])
+  }
+
+  while (tailIndex < tail.length) {
+    sortedArray.push(tail[tailIndex++])
+  }
+
+  console.log(sortedArray)
+
+  return sortedArray
+}
 
 function mergeSort (array) {
+  if (array.length < 2) return array
+
   const middle = array.length / 2
 
   const head = mergeSort(array.slice(0, middle))
-  const tail = mergeSort(array.slice(middle + 1, array.length + 1))
+  const tail = mergeSort(array.slice(middle, array.length))
 
   return merge(head, tail)
 }
--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,8 @@ const test = require('ava')
 const mergeSort = require('../src')
 
 test('Even, unordered', function (t) {
-  const input = [5, 10, 3, 1, 2, 4]
-  const expected = [1, 2, 3, 4, 5, 10]
+  const input = [5, 10, 3, 1, 2, 4, 12, 15]
+  const expected = [1, 2, 3, 4, 5, 10, 12, 15]
 
   t.deepEqual(mergeSort(input), expected)
 })
