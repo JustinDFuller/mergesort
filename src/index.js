1abe17a40b58589818e67ba7a3367e7d5b0bc494
Justin Fuller <justindanielfuller@gmail.com>
2019-04-13 10:53:01 -0400

    More tests, comments, measure iterations.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function merge (head, tail) {
+  // Use a temp array to store sorted values
   const sortedArray = []
 
+  // each array gets its own index so they can be iterated separately
   let headIndex = 0
   let tailIndex = 0
 
+  // The first time around we'll compare each index, adding the lesser
+  // value to the sorted array
   while (headIndex < head.length && tailIndex < tail.length) {
+    mergeSort.iterations++
+
     if (head[headIndex] < tail[tailIndex]) {
       sortedArray.push(head[headIndex++])
     } else {
@@ -12,28 +18,39 @@ function merge (head, tail) {
     }
   }
 
+  // add the rest of head to sorted array
   while (headIndex < head.length) {
+    mergeSort.iterations++
+
     sortedArray.push(head[headIndex++])
   }
 
+  // add rest of tail to sorted array last
   while (tailIndex < tail.length) {
+    mergeSort.iterations++
+
     sortedArray.push(tail[tailIndex++])
   }
-
-  console.log(sortedArray)
 
   return sortedArray
 }
 
 function mergeSort (array) {
+  // Arrays of length 0 or 1 are already sorted.
   if (array.length < 2) return array
 
+  // Find where to split the array
   const middle = array.length / 2
 
+  // The first half of the array, 0 through middle
   const head = mergeSort(array.slice(0, middle))
+  // The second half of the array, middle through end
   const tail = mergeSort(array.slice(middle, array.length))
 
+  // merge and sort head with tail
   return merge(head, tail)
 }
+
+mergeSort.iterations = 0
 
 module.exports = mergeSort
--- test/index.test.js
+++ test/index.test.js
@@ -6,24 +6,41 @@ test('Even, unordered', function (t) {
   const input = [5, 10, 3, 1, 2, 4, 12, 15]
   const expected = [1, 2, 3, 4, 5, 10, 12, 15]
 
+  mergeSort.iterations = 0
   t.deepEqual(mergeSort(input), expected)
+  console.log(mergeSort.iterations)
 })
 
 test('Odd, unordered', function (t) {
   const input = [5, 10, 3, 1, 2, 4, 12]
   const expected = [1, 2, 3, 4, 5, 10, 12]
 
+  mergeSort.iterations = 0
   t.deepEqual(mergeSort(input), expected)
+  console.log(mergeSort.iterations)
 })
 
 test('Even, ordered', function (t) {
   const input = [1, 2, 3, 4, 5, 10, 12, 15]
 
+  mergeSort.iterations = 0
   t.deepEqual(mergeSort(input), input)
+  console.log(mergeSort.iterations)
 })
 
 test('Odd, ordered', function (t) {
   const input = [1, 2, 3, 4, 5, 10, 12]
 
+  mergeSort.iterations = 0
   t.deepEqual(mergeSort(input), input)
+  console.log(mergeSort.iterations)
+})
+
+test('Head is always higher', function (t) {
+  const input = [10, 11, 12, 13, 1, 2, 3, 4]
+  const expected = [1, 2, 3, 4, 10, 11, 12, 13]
+
+  mergeSort.iterations = 0
+  t.deepEqual(mergeSort(input), expected)
+  console.log(mergeSort.iterations)
 })
